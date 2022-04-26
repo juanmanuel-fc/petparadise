97f1dac420c9f8d60494aa17535607a59f230645
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-04-26 19:21:55 -0300

    commit con todo el sitio listo y funcional en los 3 viewports

--- app.js
+++ app.js
@@ -25,37 +25,10 @@ app.get("/carrito", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/carrito.html"));
 });
 
-// const express = require('express');
-// const path = require("path");
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+});
 
-// const app = express();
-
-// //app.use("/static",express.static("public"));
-// app.use(express.static("public"));
-
-
-// //levantar el server
-// app.listen(3030, () =>{ 
-//     console.log("Express corriendo puerto 3030");
-// })
-
-// //rutas
-// app.get("/", (req,res) => {
-//     res.sendFile(path.join(__dirname,"/views/index.html"))
-// });
-
-// app.get("/carrito", (req,res) => {
-//     res.sendFile(path.join(__dirname,"/views/carrito.html"))
-// });
-
-// app.get("/login", (req,res) => {
-//     res.sendFile(path.join(__dirname,"/views/login.html"))
-// });
-
-// app.get("/product_view", (req,res) => {
-//     res.sendFile(path.join(__dirname,"/views/product_view.html"))
-// });
-
-// app.get("/register", (req,res) => {
-//     res.sendFile(path.join(__dirname,"/views/register.html"))
-// });
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+});
