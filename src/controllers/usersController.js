f3fa6e1c87c371a895ba05f66d13d458f98d0021
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-05-23 15:51:59 -0300

    formulario register OK

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const { Z_FIXED } = require("zlib");
+
+const { validationResult } = require("express-validator");
 
 const usersFilePath = path.join(__dirname, "../data/usuariosDataBase.json");
 const users = JSON.parse(fs.readFileSync(usersFilePath, "utf-8"));
@@ -17,37 +18,54 @@ let usersController = {
 
     createUser: function (req, res) {
 
-        // max to calculate the next ID
-        let max = 0;
+        
+        const resultValidation = validationResult(req);
 
-        for (let i=0; i<users.length; i++) {
-            if (users[i].id > max) {
-                max = users[i].id;
+        if (resultValidation.errors.length > 0) {
+            res.render("users/register", {errors: resultValidation.mapped(),
+            oldInfo: req.body
+            });
+            
+    
+        } else {
+
+            // max to calculate the next ID
+            let max = 0;
+
+            for (let i=0; i<users.length; i++) {
+                if (users[i].id > max) {
+                    max = users[i].id;
+                }
             }
+
+            let nuevoUsuario = {
+                id: max + 1,
+                nombre: req.body.nombre,
+                email: req.body.email,
+                password: req.body.password,
+                terminos: req.body.terminos
+            };
+
+            users.push(nuevoUsuario);
+
+            const usuariosJSON = JSON.stringify(users, null, "\t");
+
+            fs.writeFileSync(usersFilePath, usuariosJSON);
+
+            res.redirect("/users/list");
+
         }
 
-        let nuevoUsuario = {
-            id: max + 1,
-            nombre: req.body.nombre,
-            email: req.body.email,
-            nacimiento: req.body.nacimiento,
-            password: req.body.password,
-            terminos: req.body.terminos
-        };
-
-        users.push(nuevoUsuario);
-
-        const usuariosJSON = JSON.stringify(users, null, "\t");
-
-        fs.writeFileSync(usersFilePath, usuariosJSON);
-
-        res.redirect("/users/list");
+    
 
     },
 
     list: function (req, res) {
         res.render("users/list");
     }
+
+
+    
 }
 
 module.exports = usersController;
--- src/routes/users.js
+++ src/routes/users.js
@@ -5,13 +5,27 @@ const router = express.Router();
 //llamar al controlador
 const usersController = require("../controllers/usersController");
 
+// solo traer la propiedad body de la librería de express validator
+const { body } = require("express-validator"); 
+
+// validaciones de express-validator, todos en formato de array
+const validations = [
+    body("nombre").notEmpty().withMessage("Ingrese su nombre, el campo está vacío"),
+    body("email").isEmail().withMessage("Ingrese su email, el campo está vacío"),
+    body("nacimiento").notEmpty().withMessage("Ingrese su fecha de nacimiento, el campo está vacío"),
+    body("password").notEmpty().withMessage("Ingrese su fecha de nacimiento, el campo está vacío"),
+    body("terminos").notEmpty().withMessage("Acepte los términos y condiciones"),
+];
+
 //Crear las rutas  y ejecuta un metodo del controlador 
 router.get("/login",usersController.login);
 router.get("/register",usersController.register);
 
+// router.get("/error", usersController.error);
 
-router.post("/register", usersController.createUser);
+
 // redirection listando usuarios después de haber enviado info por POST
+router.post("/register", validations, usersController.createUser);
 router.get("/list", usersController.list);
 
 
